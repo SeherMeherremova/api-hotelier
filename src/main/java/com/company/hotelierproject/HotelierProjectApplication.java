8f2891d5b4f8002dd0f2c47a997e300eebcbd0eb
Seher <saharmaqerramova@mail.ru>
2023-04-29 18:49:55 +0400

    Initial commit

---
+++ src/main/java/com/company/hotelierproject/HotelierProjectApplication.java
@@ -0,0 +1,13 @@
+package com.company.hotelierproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelierProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelierProjectApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/company/hotelierproject/rooms/service/RoomService.java
@@ -0,0 +1,10 @@
+package com.company.hotelierproject.rooms.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RoomService {
+
+}
